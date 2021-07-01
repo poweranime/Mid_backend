ab354f8f4de3e099ea25e4e2d0748e7a99e7b6e6
poweranime <81317158+poweranime@users.noreply.github.com>
2021-07-01 16:30:53 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const mysql_pkg = require("mysql");
 
 var properties = {
-    host: "34.126.172.116",
+    host: "xxxxx",
     user: "root",
-    password: "fintechsglab",
+    password: "xxxx",
     port: 3306,
     database: "market",
 }
